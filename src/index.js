6ceb1262ff3130e8394f31d7cc6dfac55b883e8d
faresh9 <93577791+faresh9@users.noreply.github.com>
2023-08-27 21:56:04 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import HoroscopeContext from "./context/HoroscopeContext";
 const Root = () => {
   return (
+    <HoroscopeContext.Providor>
       <App />
+    </HoroscopeContext.Providor>
   );
 };
 
@@ -13,5 +15,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
